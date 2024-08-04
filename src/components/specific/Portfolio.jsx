de44994b5bb9cf625ec4aadefc50ed1ece28ae55
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-08-04 07:43:22 +0100

    portfolio component

---
+++ src/components/specific/Portfolio.jsx
@@ -0,0 +1,12 @@
+import './Portfolio.css';
+
+const Portfolio = () => {
+  return (
+    <div className="portfolio">
+      <h2>My Projects</h2>
+      {/* Add your portfolio items here */}
+    </div>
+  );
+};
+
+export default Portfolio;
