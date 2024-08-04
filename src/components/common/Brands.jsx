eaf2d8c3442123479b08d0da8cf3c4cd24458295
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-08-04 07:44:15 +0100

    brand component

---
+++ src/components/common/Brands.jsx
@@ -0,0 +1,14 @@
+export default function Brands() {
+  const brandStyle = {
+    height: "70px",
+    backgroundColor: "#121212",
+    position: "absolute",
+    bottom: "0px",
+    width: "100%",
+  };
+  return (
+    <>
+      <div style={brandStyle}></div>
+    </>
+  );
+}
