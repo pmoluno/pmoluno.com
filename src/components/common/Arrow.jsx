90faefd0ee6ecc6e0c5dd5920046e3d512db2f35
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-08-04 07:43:43 +0100

    arrow component

---
+++ src/components/common/Arrow.jsx
@@ -0,0 +1,15 @@
+export default function Arrow(){
+    const iconStyle = {
+        padding: '15px',
+        fontSize: '20px',
+        borderRadius: '50%',
+        backgroundColor: '#f47e05',
+        border: '2px solid #ffffff',
+        color: '#000000',
+      };
+    return(
+        <>
+         <i className="bi bi-arrow-up-right" style={iconStyle}></i>
+        </>
+    )
+}
