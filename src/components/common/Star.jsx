60cc710dd76e5287da1f1cc9a022427893f64343
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-08-04 07:43:58 +0100

    star component

---
+++ src/components/common/Star.jsx
@@ -0,0 +1,11 @@
+export default function Star(){
+    const iconStyle ={
+        fontSize: '60px',
+        color: '#f47e05',
+        position: 'relative',
+        bottom: '30px',
+    }
+    return(
+        <><i class="bi bi-stars" style={iconStyle}></i></>
+    )
+}
