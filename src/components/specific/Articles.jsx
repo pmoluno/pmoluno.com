f48deef8dde8926101c43d9488ba9041a802b6bc
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-08-04 07:44:52 +0100

    article component

---
+++ src/components/specific/Articles.jsx
@@ -0,0 +1,12 @@
+import './Articles.css';
+
+const Articles = () => {
+  return (
+    <div className="articles">
+      <h2>My Articles</h2>
+      {/* Add your articles or blog posts here */}
+    </div>
+  );
+};
+
+export default Articles;
