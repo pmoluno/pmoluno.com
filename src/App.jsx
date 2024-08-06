37ee3b89beba5d1726a377083bd69bcd9a5f4c4b
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-08-06 21:38:07 +0100

    updated app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import ReactFullpage from "@fullpage/react-fullpage";
 import Navbar from "./components/specific/Navbar";
 import Hero from "./components/specific/Hero";
 import About from "./components/specific/About";
@@ -14,31 +13,10 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <ReactFullpage
-        navigationTooltips={["Intro", "About Me", "Projects", "Articles"]}
-        navigation={true}
-        slidesNavigation={true}
-        slidesNavPosition="bottom"
-        controlArrows={true}
-        licenseKey="xxxxxxxxxxxxxxxxxxxxxxxxx"
-        render={() => (
-          <ReactFullpage.Wrapper>
-            <div className="section hero-section">
-              <Hero />
-              <Brands />
-            </div>
-            <div className="section about-section">
-              <About />
-            </div>
-            <div className="section portfolio-section">
-              <Portfolio />
-            </div>
-            <div className="section articles-section">
-              <Articles />
-            </div>
-          </ReactFullpage.Wrapper>
-        )}
-      />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Articles />
       <Footer />
     </div>
   );
